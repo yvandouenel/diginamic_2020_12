6a1fc27e5f41f18fbcfc94b8018e98d75081bb79
yvan <y.douenel@coopernet.fr>
2020-12-17 14:23:52 +0100

    ennoncé de l'exercice show hide.

---
+++ testJs/js/testjs14-event-show-hide.js
@@ -0,0 +1,11 @@
+/**
+ * Faire en sorte que le click sur le titre h1
+ * cache ou fasse apparaître le paragraphe qui se trouve
+ * juste après le titre de niveau 1
+ * 
+ * 1 - ajouter un event handler sur l'élément DOM qui correspond à h1
+ * 2 - Dans ce event handler, on va tester si le paragraphe est visible. 
+ *     S'il est visible, le cacher (visibility hidden) sinon le montrer (visibility  visible)
+ *     Cela sous-entend que vous avez trouvé comment modifier des propriétés css via 
+ *     du javascript
+ */
