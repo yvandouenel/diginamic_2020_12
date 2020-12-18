ba0056e35cfe35e55644e11698d332d3805689b3
yvan <y.douenel@coopernet.fr>
2020-12-18 09:45:35 +0100

    ennoncé pour la suite de l'exercice de conversion.

--- testJs/js/testjs16-convertisseur.js
+++ testJs/js/testjs16-convertisseur.js
@@ -15,6 +15,16 @@ form.onsubmit = function(event) {
 
 }
 
+// gestion de la modification des inputs
+// récupération des élément du DOM qui correspondent aux inputs 
+// avec query selector par exemple
+
+
+// affectation d'une fonction gestionnaire d'événement (input)
+// qui va appeler la fonction convert et qui va modifier
+// l'input des francs suisse (chf) ou l'input des euros 
+
+
 function convert(amount, currency) {
   if(currency == "chf") return amount * 1.06;
   else return amount / 1.06;
