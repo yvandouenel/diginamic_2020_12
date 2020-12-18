00a26c4868b61644707a19855c97300b191094be
yvan <y.douenel@coopernet.fr>
2020-12-18 16:38:58 +0100

    Card.js avec la gestion de la modification d'une carte

--- testJs/js/classes/Card.js
+++ testJs/js/classes/Card.js
@@ -10,12 +10,41 @@ export default class Card extends CoopDom {
         this.domElements = this.render();
 
         // gestion des événements
+        this.handleEvents();
+
+    }
+    handleEvents = () => {
+        // suppression d'une carte
         this.domElements.button_remove.onclick = () => {
             console.log("click sur le bouton pour supprimer une carte");
+            console.log("this dans onclick du bouton de suppression de carte : ", this);
             this.column.removeCard(this);
         }
 
-        
+        // affichage du formulaire au click sur le bouton modifier
+        this.domElements.button_edit.onclick = () => {
+            this.domElements.form_edit.hidden = false;
+        }
+
+        // gestion de la soumission du formulaire de modification
+        this.domElements.form_edit.onsubmit = (event) => {
+            console.log("Gestion de la soumission du formulaire de modification");
+            event.preventDefault();
+
+            // Récupération des nouvelles valeurs
+            const new_question = this.domElements.input_question.value;
+            const new_answer = this.domElements.input_answer.value;
+
+            // Modification à la fois des propriétés question et answer
+            // mais aussi des éléments du dom correspondant
+            this.question = new_question;
+            this.domElements.question.textContent = new_question;
+            this.answer = new_answer;
+            this.domElements.answer.textContent = new_answer;
+
+            // on cache le formulaire
+            this.domElements.form_edit.hidden = true;
+        }
     }
     render = () => {
         console.log("Dans la fonction render de Card");
@@ -39,22 +68,71 @@ export default class Card extends CoopDom {
             this.answer,
             article
         );
+        
+        // création du formulaire
+        const form_edit = this.createAddDomElt(
+            "form",
+            "",
+            article
+        );
+        const label_question = this.createAddDomElt(
+            "label",
+            "Question",
+            form_edit
+        );
+        const input_question = this.createAddDomElt(
+            "input",
+            "",
+            form_edit,
+            {"type": "text", "value": this.question}
+        );
+        const label_answer = this.createAddDomElt(
+            "label",
+            "Réponse",
+            form_edit
+        );
+        const input_answer = this.createAddDomElt(
+            "input",
+            "",
+            form_edit,
+            {"type": "text", "value": this.answer}
+        );
+        const button_submit_edit = this.createAddDomElt(
+            "input",
+            "",
+            form_edit,
+            {"type": "submit", "value": "Modifier"}
+        );
+        // on cache le formulaire
+        form_edit.hidden = true;
 
+        // création des boutons
         const button_remove = this.createAddDomElt(
             "button",
             "Supprimer la carte",
             article
         );
 
+
         /**
          * Créer un bouton qui va afficher au click un formulaire
          * qui permettra de modifier la carte (la question et/ou la réponse )
          */
+        const button_edit = this.createAddDomElt(
+            "button",
+            "Modifier la carte",
+            article
+        );
+        
         return {
             "article": article,
             "question": question,
             "answer": answer,
-            "button_remove": button_remove
+            "button_remove": button_remove,
+            "form_edit": form_edit,
+            "input_answer": input_answer,
+            "input_question": input_question,
+            "button_edit": button_edit,
         };
 
     }
