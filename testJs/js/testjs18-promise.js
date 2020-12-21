3cea4c00dd6ee52e19adcbc8a9775c107a07775b
yvan <y.douenel@coopernet.fr>
2020-12-21 11:19:49 +0100

    Promesses avec async et await.

--- testJs/js/testjs18-promise.js
+++ testJs/js/testjs18-promise.js
@@ -1,7 +1,10 @@
 function getToken() {
     return new Promise(function (resolve, reject) {
         if (Math.random() > 0.5) {
-            resolve("Xdl586655zeEszzs");
+            // l'argument "Xdl586655zeEszzs" devient le premier paramètre de la fonction qui
+            // est en paramatre du prochain "then"
+            resolve("Xdl586655zeEszzs"); 
+
         }
         else {
             reject("Erreur dans l'obtention du token");
@@ -20,7 +23,7 @@ function getUser() {
     });
 
 }
-getToken()
+/* getToken()
     .then(function (data) {
         console.log("Cas favorable, token : ", data);
         // chaînage possible car getUser renvoie également une promesse
@@ -31,5 +34,20 @@ getToken()
     })
     .catch(function (error) {
         console.error(error);
-    });
+    }); */
+
+async function getTokenUser() { // async prévient que on va manipuler des fonction asynchrone
+    try {
+        // token correpond à l'argument renvoyé par la fonction resolve de getToken()
+        const token = await getToken(); 
+        console.log("token : ", token);
+        // récupération des infos de l'utilisateur si la récupération du code s'est bien passée
+        const user = await getUser();
+        console.log("user : ", user);
+    } catch (error) {
+        console.log('Erreur attrapée : ', error);
+    }
+}
+getTokenUser();
+
 
